52e3fa2b0847ef10bc666066b1bde7adb1ad3996
Shengzhe Chen <schen@ancestry.com>
2014-10-31 10:40:47 -0700

    adding projects

---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+
+	private int count;
+
+	void Start() {
+		count = 0;
+		UpdateCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate() {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Pickup") {
+			other.gameObject.SetActive(false);
+			count ++;
+			UpdateCountText();
+
+			if (count >= 12) {
+				winText.text = "YOU WIN!";
+			}
+		}
+	}
+
+	private void UpdateCountText() {
+		countText.text = "Count: " + count.ToString();
+	}
+}
