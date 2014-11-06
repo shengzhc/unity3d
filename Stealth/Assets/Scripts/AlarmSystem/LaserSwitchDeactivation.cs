aaf4abb8f03fc7b4f4170d8761a44ea9bc55d88c
Shengzhe Chen <schen@ancestry.com>
2014-11-05 23:48:13 -0800

    adding fens and terminals

---
+++ Stealth/Assets/Scripts/AlarmSystem/LaserSwitchDeactivation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserSwitchDeactivation : MonoBehaviour 
+{
+	public GameObject laser;
+	public Material unlockMaterial;
+
+	private GameObject player;
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag (Tags.player);
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		if (other.gameObject == player) {
+			if (Input.GetButton ("Switch")) {
+				LaserDeactivation ();
+			}
+		}
+	}
+
+	void LaserDeactivation ()
+	{
+		laser.SetActive (false);
+		Renderer screen = transform.Find ("prop_switchUnit_screen").renderer;
+		screen.material = unlockMaterial;
+		audio.Play ();
+	}
+}
