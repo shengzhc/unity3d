858eb343fc683916f0c182edfe0b83ca57a39aa4
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-11-09 13:30:33 -0800

    add lift

---
+++ Stealth/Assets/Scripts/LevelDynamics/DoorAnimation.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAnimation : MonoBehaviour 
+{
+	public bool requireKey;
+	public AudioClip doorSwitchClip;
+	public AudioClip doorDeniedClip;
+
+	private GameObject player;
+	private Animator anim;
+	private PlayerInventory playerInventory;
+	private HashIDs hash;
+	private int count;
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag (Tags.player);
+		anim = GetComponent<Animator> ();
+		playerInventory = player.GetComponent<PlayerInventory> ();
+		hash = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<HashIDs> ();
+	}
+
+	void Update ()
+	{
+		anim.SetBool (hash.openBool, count > 0);
+
+		if (anim.IsInTransition (0) && !audio.isPlaying) {
+			audio.clip = doorDeniedClip;
+			audio.Play ();
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject == player) {
+			if (requireKey) {
+				if (playerInventory.hasKey) {
+					count++;
+				} else {
+					audio.clip = doorDeniedClip;
+					audio.Play ();
+				}
+			} else {
+				count++;
+			}
+		} else if (other.gameObject.tag == Tags.enemy) {
+			if (other is CapsuleCollider) {
+				count++;
+			}
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject == player || (other.gameObject.tag == Tags.enemy && other is CapsuleCollider)) {
+			count = Mathf.Max (0, count-1);
+		}
+	}
+}
---
+++ Stealth/Assets/Scripts/LevelDynamics/LiftTrigger.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LiftTrigger : MonoBehaviour 
+{
+	public float timeToDoorClose = 2f;
+	public float timeToLiftStart = 3f;
+	public float timeToEndLevel = 6f;
+	public float liftSpeed = 3f;
+
+	private GameObject player;
+	private Animator playerAnim;
+	public HashIDs hash;
+	private CameraMovement cameraMovement;
+	private SceneFadeInOut sceneFadeInOut;
+	private LiftDoorTracking liftDoorTracking;
+	private bool playerInLift;
+	private float timer;
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag (Tags.player);
+		playerAnim = player.GetComponent<Animator> ();
+		hash = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<HashIDs> ();
+		cameraMovement = Camera.main.GetComponent<CameraMovement> ();
+		sceneFadeInOut = GameObject.FindGameObjectWithTag (Tags.fader).GetComponent<SceneFadeInOut> ();
+		liftDoorTracking = GetComponent<LiftDoorTracking> ();
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject == player) {
+			playerInLift = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other) 
+	{
+		if (other.gameObject == player) {
+			playerInLift = false;
+			timer = 0.0f;
+		}
+	}
+
+	void Update ()
+	{
+		if (playerInLift) {
+			LiftActivation ();
+		}
+
+		if (timer < timeToDoorClose) {
+			liftDoorTracking.DoorFollowing ();
+		} else {
+			liftDoorTracking.CloseDoors ();
+		}
+	}
+
+	void LiftActivation ()
+	{
+		timer += Time.deltaTime;
+
+		if (timer >= timeToLiftStart)
+		{
+			playerAnim.SetFloat (hash.speedFloat, 0.0f);
+			cameraMovement.enabled = false;
+			player.transform.parent = transform;
+
+			transform.Translate (Vector3.up * liftSpeed * Time.deltaTime);
+
+			if (!audio.isPlaying) {
+				audio.Play ();
+			}
+		}
+
+		if (timer >= timeToEndLevel) {
+			sceneFadeInOut.EndScene ();
+		}
+	}
+}
