37001ec91c68840265270b9d3344d35a41f0e81e
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-11-14 13:23:46 -0800

    Add Yoon project

---
+++ Yoon/Assets/Assets/Game/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour 
+{
+	public float rotateSpeed = 60.0f;
+
+	void Update ()
+	{
+		transform.Rotate (transform.up, rotateSpeed * Time.deltaTime);
+	}
+}
