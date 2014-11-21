8c81a7bc69227037842751e63768d78ab2c8e9b1
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-11-21 09:34:09 -0800

    update

--- Yoon/Assets/Assets/Game/Scripts/Player/YoonMovement.cs
+++ Yoon/Assets/Assets/Game/Scripts/Player/YoonMovement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class YoonMovement : MonoBehaviour 
 {
 	public float speed = 5.0f;
-	public float speedDamping = 0.5f;
+	public float speedSmoothing = 0.5f;
 	void Update () 
 	{
 		float h = Input.GetAxis ("Horizontal");
@@ -16,13 +16,16 @@ public class YoonMovement : MonoBehaviour
 	void Moving (float h, float v) 
 	{
 		if (Mathf.Abs(h) > 0 || Mathf.Abs(v) > 0) {
-			Vector3 targetDirection = new Vector3 (h, 0, v).normalized * speed;
-			transform.position = Vector3.Lerp (transform.position, targetDirection + transform.position, speedDamping * Time.deltaTime);
+			Vector3 target = new Vector3 (h, 0, v).normalized * speed + transform.position;
+			transform.position = Vector3.Lerp (transform.position, target, speedSmoothing * Time.deltaTime);
 		}
 	}
 
 	void Rotating (float h, float v) 
 	{
+		if (Mathf.Abs (h) > 0 || Mathf.Abs (v) > 0) {
+			Vector3 lookAtDirection = new Vector3 (h, 0, v).normalized;
 
+		}
 	}
 }
