5ee69977adc9fca86648489f57c0685395b18e02
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-11-21 07:37:22 -0800

    add yoon movement

---
+++ Yoon/Assets/Assets/Game/Scripts/Player/YoonAnimationState.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class YoonAnimationState : MonoBehaviour 
+{
+	private List<string> animationClips = new List<string> ();
+	private int index = 0;
+
+	public enum YoonAnimationType 
+	{
+		IDLE = 0,
+		STAND,
+		WALK,
+		RUN,
+		JUMP,
+		POSE,
+
+		AOQ_IDLE,
+		AOQ_WALK,
+		AOQ_HIT,
+		AOQ_GLAD,
+		AOQ_REST_A,
+		AOQ_REST_B,
+		AOQ_WARP,
+
+		FLY_IDLE,
+		FLY_UP,
+		FLY_DOWN,
+		FLY_STRAIGHT,
+		FLY_TOLEFT,
+		FLY_TORIGHT,
+		FLY_DAMAGE,
+		FLY_DISAPPO,
+		FLY_DISAPPO_LOOP,
+		FLY_ITEMGET,
+		FLY_ITEMGET_LOOP,
+
+		HW_IDLE,
+		HW_WAIT_LONG,
+		HW_TRICK,
+		HW_MAHOU
+	}
+
+	void Awake ()
+	{
+		foreach (AnimationState animationState in animation) {
+			animationClips.Add (animationState.name);
+		}
+		animationClips.Sort ();
+	}
+}
---
+++ Yoon/Assets/Assets/Game/Scripts/Player/YoonMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YoonMovement : MonoBehaviour 
+{
+	public float speed = 5.0f;
+	public float speedDamping = 0.5f;
+	void Update () 
+	{
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		Moving (h, v);
+	}
+
+	void Moving (float h, float v) 
+	{
+		if (Mathf.Abs(h) > 0 || Mathf.Abs(v) > 0) {
+			Vector3 targetDirection = new Vector3 (h, 0, v).normalized * speed;
+			transform.position = Vector3.Lerp (transform.position, targetDirection + transform.position, speedDamping * Time.deltaTime);
+		}
+	}
+
+	void Rotating (float h, float v) 
+	{
+
+	}
+}
